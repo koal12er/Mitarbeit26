e62f71cab12fa22774e6a1b04b4f129c4470f6b8
Felix <146081021+koal12er@users.noreply.github.com>
2025-10-21 12:31:04 +0200

    verson 1

--- SEw/Indexer/Indexer/Formel1Piloten.cs
+++ SEw/Indexer/Indexer/Formel1Piloten.cs
@@ -2,5 +2,27 @@ namespace Indexer;
 
 public class Formel1Piloten
 {
-    
+    private const int MAX_PILOTS = 99;
+    Person[] _piloten = new Person[MAX_PILOTS];
+
+    public Person this[int i]
+    {
+        get{return _piloten[i-1];}
+        set{_piloten[i-1]=value;}
+    }
+
+    public Person this[string s]
+    {
+        get
+        {
+            for (int i = 0; i < MAX_PILOTS; i++)
+            {
+                if (_piloten[i] != null)
+                    if(_piloten[i].ToString()==s)
+                        return _piloten[i];
+            }
+            return null;
+        }
+    }
+        
 }
--- SEw/Indexer/Indexer/Program.cs
+++ SEw/Indexer/Indexer/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using Indexer;
+// Neues Formel1Piloten-Objekt
+Formel1Piloten f1 = new Formel1Piloten();
+
+// Ein paar Piloten hinzufügen
+f1[1] = new Person { FirstName = "Max", LastName = "Verstappen" };
+f1[2] = new Person { FirstName = "Charles", LastName = "Leclerc" };
+f1[3] = new Person { FirstName = "Lando", LastName = "Norris" };
+
+// Zugriff über Index
+Console.WriteLine("1. Pilot: " + f1[1]);
+
+// Zugriff über String (Name)
+Console.WriteLine("Suche nach 'Charles Leclerc': " + f1["Charles Leclerc"]);
+Console.WriteLine("Suche nach 'Lando Norris': " + f1["Lando Norris"]);
+
+// Versuch, jemanden zu finden, der nicht existiert
+var unknown = f1["Lewis Hamilton"];
+if (unknown == null)
+    Console.WriteLine("Lewis Hamilton wurde nicht gefunden 😅");
