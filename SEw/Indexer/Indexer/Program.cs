b9949a09a19bfd3324620f60582ff347d8182111
Felix <146081021+koal12er@users.noreply.github.com>
2025-10-22 12:33:06 +0200

    commentare

--- SEw/Indexer/Indexer/Program.cs
+++ SEw/Indexer/Indexer/Program.cs
@@ -1,45 +1,47 @@
-﻿
+﻿using Indexer;
 
-using Indexer;
-// Neues Formel1Piloten-Objekt
+// Neues Formel1Piloten-Objekt anlegen
 Formel1Piloten f1 = new Formel1Piloten();
 
-// Ein paar Piloten hinzufügen
+// Ein paar Piloten hinzufügen (über den int-Indexer)
 f1[1] = new Person { FirstName = "Max", LastName = "Verstappen" };
 f1[2] = new Person { FirstName = "Charles", LastName = "Leclerc" };
 f1[3] = new Person { FirstName = "Lando", LastName = "Norris" };
 
-// Zugriff über Index
+// Zugriff über Index (1-basiert)
 Console.WriteLine("1. Pilot: " + f1[1]);
 Console.WriteLine("------------------------------------");
-// Zugriff über String (Name)
+
+// Zugriff über String (vollständiger Name)
 Console.WriteLine("Suche nach 'Charles Leclerc': " + f1["Charles Leclerc"]);
 Console.WriteLine("Suche nach 'Lando Norris': " + f1["Lando Norris"]);
 Console.WriteLine("------------------------------------");
-// Versuch, jemanden zu finden, der nicht existiert
+
+// Versuch, jemanden zu finden, der noch nicht existiert
 var unknown = f1["Lewis Hamilton"];
 if (unknown == null)
     Console.WriteLine("Lewis Hamilton wurde nicht gefunden 😅");
 else
-{
     Console.WriteLine("Lewis Hamilton wurde gefunden ");
-}
 
-
-f1["Lewis Hamilton"] = new Person { FirstName = "Lewis", LastName = "Hamilton" };    
+// Neuen Piloten hinzufügen (über String-Indexer)
+f1["Lewis Hamilton"] = new Person { FirstName = "Lewis", LastName = "Hamilton" };
 Console.WriteLine("------------------------------------");
-// Versuch, jemanden zu finden, der existiert
+
+// Jetzt nochmal versuchen, Lewis Hamilton zu finden
 var unknown1 = f1["Lewis Hamilton"];
 if (unknown1 == null)
     Console.WriteLine("Lewis Hamilton wurde nicht gefunden 😅");
 else
-{
     Console.WriteLine("Lewis Hamilton wurde gefunden ");
-}
 
 Console.WriteLine("------------------------------------");
+
+// Ganze Liste aller Piloten ausgeben
 Console.WriteLine(f1);
 Console.WriteLine("------------------------------------");
+
+// Alle vorhandenen Piloten per Schleife (Index) ausgeben
 for (int b = 0; b < 99; b++)
 {
     if (f1[b] != null)
@@ -48,9 +50,9 @@ for (int b = 0; b < 99; b++)
     }
 }
 
-foreach (var person in f1 )
+// Alle Piloten per foreach-Schleife (da IEnumerable implementiert)
+foreach (var person in f1)
 {
     if (person != null)
         Console.WriteLine(person);
 }
-
