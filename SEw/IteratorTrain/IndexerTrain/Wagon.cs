e2ac92325bab91df3679a4b44b8e97967a05f513
Felix <146081021+koal12er@users.noreply.github.com>
2025-11-11 12:28:32 +0100

    .

--- SEw/IteratorTrain/IndexerTrain/Wagon.cs
+++ SEw/IteratorTrain/IndexerTrain/Wagon.cs
@@ -4,9 +4,12 @@ public class Wagon
 {
     public string Color { get; set; }
     public int Capacity { get; set; }
-    public Wagon? Next;
+    public Wagon Next;
 
- 
 
-    public override string ToString() => Color;
+
+    public override string ToString()
+    {
+        return $"Wagon {Color} , capacity {Capacity}";
+    }
 }
